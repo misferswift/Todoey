eb7ce87a48fa9fda399d8f29120820e098dc16b7
zaher salem ali alGuerni <Dano@danos-macbook-pro.local>
2019-02-19 09:44:37 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Danah Algarni on 2/19/19.
+//  Copyright © 2019 MisferSwift. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
